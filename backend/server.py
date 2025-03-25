7aade99165e3e2bca2e4fe9230e38015d0953f36
Robin Singh Chouhan <robinsingh.02@gmail.com>
2025-03-25 22:03:29 +1100

    Added GEMINI_API_KEY

--- backend/server.py
+++ backend/server.py
@@ -4,19 +4,18 @@ from dotenv import load_dotenv
 import os
 app = Flask(__name__)
 
-
 load_dotenv()
-APIKEY = ""
+
+APIKEY = os.getenv('GEMINI_API_KEY')
 
 genai.configure(api_key=APIKEY)
 
 model = genai.GenerativeModel(model_name='gemini-1.5-flash')
 
-@app.route('/api/hello',methods=['GET'])
-def hello():
-    response = model.generate_content('1+1')
-    print(response.text)
-    return jsonify({'message:':'Hello World'})
+@app.route('/api/question/<question>',methods=['GET'])
+def hello(question):
+    response = model.generate_content(str(question))
+    return jsonify({'message:':response.text})
 
 app.run()
 
