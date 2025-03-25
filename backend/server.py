45e510cbd314edcd5c7391351d3504a01f407ca7
Robin Singh Chouhan <robinsingh.02@gmail.com>
2025-03-25 23:13:24 +1100

    Connected Android App to Gemini

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
@@ -12,14 +12,14 @@ genai.configure(api_key=APIKEY)
 
 model = genai.GenerativeModel(model_name='gemini-1.5-flash')
 
-@app.route('/api/question/<question>',methods=['GET'])
-def hello(question):
+@app.route('/',methods=['GET'])
+def hello():
+    return jsonify({'message:':'Server is runnung!'})
+
+@app.route('/api/gemini/<question>',methods=['GET'])
+def geminiResponse(question):
     response = model.generate_content(str(question))
+    print(response)
     return jsonify({'message:':response.text})
 
 app.run()
-
-
-
-
-
---
+++ mobile/GeminiOCR/app/src/main/java/com/example/geminiocr/GeminiInterface.kt
@@ -0,0 +1,4 @@
+package com.example.geminiocr
+
+class GeminiInterface {
+}
